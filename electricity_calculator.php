996053899a0cf33e1883b67814b0811beb755425
Adam Zaidan <87437190+adamzaidanaiyob@users.noreply.github.com>
2023-08-08 15:57:06 +0800

    Update electricity_calculator.php

--- electricity_calculator.php
+++ electricity_calculator.php
@@ -1,23 +1,39 @@
 <?php
+function calculateElectricityCost($voltage, $current, $rate) {
+    $power = ($voltage * $current) / 1000; // Moving the decimal point three times to the left for kWh unit conversion
+    $energy = $power * 1 * 1000; // Assuming 1 hour
+    $totalCharge = $energy * ($rate / 100);
+
+    // Convert rate to MYR (sen/kWh)
+    $ratePerHour = ($totalCharge / $energy) * 100; // Rate per kWh
+    $ratePerDay = $ratePerHour * 24; // Rate per day
+
+    $hourlyCharges = array();
+    for ($hour = 1; $hour <= 24; $hour++) {
+        $hourlyEnergy = $energy * $hour;
+        $hourlyCharge = $hourlyEnergy * ($rate / 100000); // conversion from sen to RM
+        $hourlyCharges[] = array(
+            'hour' => $hour,
+            'hourlyEnergy' => $hourlyEnergy / 1000,
+            'hourlyCharge' => $hourlyCharge,
+        );
+    }
+
+    return array(
+        'power' => $energy / 1000,
+        'ratePerHour' => $ratePerHour / 100,
+        'hourlyCharges' => $hourlyCharges,
+    );
+}
+
 $voltage = isset($_POST["voltage"]) ? $_POST["voltage"] : "";
 $current = isset($_POST["current"]) ? $_POST["current"] : "";
 $rate = isset($_POST["rate"]) ? $_POST["rate"] : "";
 
-$power = "";
-$energy = "";
-$totalCharge = "";
-$ratePerHour = "";
-$ratePerDay = "";
-
+$results = array();
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     if (!empty($voltage) && !empty($current) && !empty($rate)) {
-        $power = ($voltage * $current) / 1000; // Moving the decimal point three times to the left for kWh unit conversion
-        $energy = $power * 1 * 1000; // Assuming 1 hour
-        $totalCharge = $energy * ($rate / 100);
-
-        // Convert rate to MYR (sen/kWh)
-        $ratePerHour = ($totalCharge / $energy) * 100; // Rate per kWh
-        $ratePerDay = $ratePerHour * 24; // Rate per day
+        $results = calculateElectricityCost($voltage, $current, $rate);
     }
 }
 ?>
@@ -30,41 +46,41 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <title>Electricity Calculator</title>
     <style>
-    body {
-        padding: 20px;
-        font-family: Times New Roman;
-    }
-    .container {
-        max-width: 800px;
-        margin: auto;
-    }
-    table {
-        width: 100%;
-        margin-top: 20px;
-        border-collapse: collapse;
-    }
-    th, td {
-        padding: 12px;
-        text-align: center;
-    }
-    th {
-        background-color: #3498db; /* Bold Blue Color */
-        color: #ffffff; /* Text Color for Bold Blue */
-        font-weight: bold;
-    }
-    td {
-        background-color: #fff;
-    }
-    th, td {
-        border: 1px solid #ddd;
-    }
-    tr:nth-child(even) td {
-        background-color: #f2f2f2;
-    }
-    .table-container {
-        margin-top: 30px;
-    }
-</style>
+        body {
+            padding: 20px;
+            font-family: Times New Roman;
+        }
+        .container {
+            max-width: 800px;
+            margin: auto;
+        }
+        table {
+            width: 100%;
+            margin-top: 20px;
+            border-collapse: collapse;
+        }
+        th, td {
+            padding: 12px;
+            text-align: center;
+        }
+        th {
+            background-color: #3498db; /* Bold Blue Color */
+            color: #ffffff; /* Text Color for Bold Blue */
+            font-weight: bold;
+        }
+        td {
+            background-color: #fff;
+        }
+        th, td {
+            border: 1px solid #ddd;
+        }
+        tr:nth-child(even) td {
+            background-color: #f2f2f2;
+        }
+        .table-container {
+            margin-top: 30px;
+        }
+    </style>
 </head>
 <body>
     <div class="container">
@@ -85,10 +101,9 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
             <button type="submit" class="btn btn-primary">Calculate</button>
         </form>
 
-        <?php if (!empty($power)) { ?>
-           <p> </p> 
-            <p>POWER: <?php echo number_format($energy / 1000, 5); ?> kW</p>
-            <p>RATE: <?php echo number_format($ratePerHour / 100, 3); ?> RM</p>
+        <?php if (!empty($results)) { ?>
+            <p>POWER: <?php echo number_format($results['power'], 5); ?> kW</p>
+            <p>RATE: <?php echo number_format($results['ratePerHour'], 3); ?> RM</p>
             <table>
                 <tr>
                     <th># Hour</th>
@@ -96,13 +111,11 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
                     <th>TOTAL (RM)</th>
                 </tr>
                 <?php
-                for ($hour = 1; $hour <= 24; $hour++) {
-                    $hourlyEnergy = $energy * $hour;
-                    $hourlyCharge = $hourlyEnergy * ($rate / 100000); // conversion from sen to RM
+                foreach ($results['hourlyCharges'] as $hourlyCharge) {
                     echo "<tr>";
-                    echo "<td>{$hour}</td>";
-                    echo "<td>" . number_format($hourlyEnergy / 1000, 5) . "</td>";
-                    echo "<td>" . number_format($hourlyCharge, 2) . "</td>";
+                    echo "<td>{$hourlyCharge['hour']}</td>";
+                    echo "<td>" . number_format($hourlyCharge['hourlyEnergy'], 5) . "</td>";
+                    echo "<td>" . number_format($hourlyCharge['hourlyCharge'], 2) . "</td>";
                     echo "</tr>";
                 }
                 ?>
